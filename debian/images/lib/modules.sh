6bedd166c817a1b37d8287b7af1a396107581ef7
cedric_nanni <cedric.nanni@aprilsoftware.com>
2024-03-05 15:19:51 +0100

    Modules improvments

--- debian/images/lib/modules.sh
+++ debian/images/lib/modules.sh
@@ -6,13 +6,9 @@
 # See the LICENSE file in the top-level directory.
 #
 
-echo "MODULES: ${MODULES}"
-
 if [ ! -z "${MODULES}" ]
 then
   for MODULE in `echo "${MODULES}" | grep -o -e "[^,]*"`; do
-      echo "MODULE: ${MODULE}"
-
       source ${LIB_PATH}/modules/${MODULE}.sh
   done
 fi
