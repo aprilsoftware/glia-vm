25b64c0cdc877910893a09b151130bb3941c852f
cedric_nanni <cedric.nanni@aprilsoftware.com>
2024-03-05 15:20:34 +0100

    Modules improvments

--- debian/images/lib/modules/kubernetes/install_1.29.sh
+++ debian/images/lib/modules/kubernetes/install_1.29.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2023 April Software S.A.
+# Copyright (C) 2024 April Software S.A.
 #
 # This work is licensed under the GNU GPLv3 or later.
 # See the LICENSE file in the top-level directory.
