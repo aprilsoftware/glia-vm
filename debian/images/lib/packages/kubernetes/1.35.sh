ddda8461502f47bf2dfeca712938b98f1ba5bee1
cedric_nanni <cedric.nanni@aprilsoftware.com>
2026-07-02 11:20:34 +0200

    New Kubernetes versions

---
+++ debian/images/lib/packages/kubernetes/1.35.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Copyright (C) 2026 April Software S.A.
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+KUBERNETES_VERSION=v1.35
+PROJECT_PATH=stable:/v1.35
+
+apt install gnupg2 apt-transport-https curl iptables -y
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf 
+overlay 
+br_netfilter 
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf 
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1 
+EOF
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/$PROJECT_PATH/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/$PROJECT_PATH/deb/ /" | tee /etc/apt/sources.list.d/cri-o.list
+
+apt update
+
+apt install -y cri-o kubelet kubeadm kubectl
